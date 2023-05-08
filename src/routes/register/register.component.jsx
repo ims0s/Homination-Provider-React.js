e346d33809441a2f936d224397d796cc59a532d7
ims0's laptop <soruor2001@gmail.com>
2023-05-08 12:44:25 +0300

    finishing all of the auth work

--- src/routes/register/register.component.jsx
+++ src/routes/register/register.component.jsx
@@ -8,6 +8,8 @@ import Popover from 'react-bootstrap/Popover';
 import '../Login/login.style.css'
 import axios from "axios";
 import './register.style.css'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 class Register extends Component{
     constructor(){
         super();
@@ -101,64 +103,70 @@ class Register extends Component{
                
                <Form className="authForm" noValidate validated={validate} onSubmit={submitHandler}>
                 <h1> Homination</h1>
-               <Form.Group className="mb-3" controlId="username">
-                    <Form.Label>Username</Form.Label>
-                    <Form.Control type="text" placeholder="Username" onChange={onChangeHandler} required/>
-                    <Form.Control.Feedback type="invalid">
-                        Enter your Username!!
-                    </Form.Control.Feedback>
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="name">
-                    <Form.Label>Name</Form.Label>
-                    <Form.Control type="text" placeholder="Name" onChange={onChangeHandler} required/>
-                    <Form.Control.Feedback type="invalid">
-                        Enter your Name!!
-                    </Form.Control.Feedback>
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="email">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="example@email.com" onChange={onChangeHandler}   required />
-                    <Form.Control.Feedback type='invalid' >
-                    Enter your email like this : example@email.com
-                    </Form.Control.Feedback>
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="phone_numbe">
-                    <Form.Label>Phone Nubmer</Form.Label>
-                    <Form.Control type="text" placeholder="Name" onChange={onChangeHandler} required/>
-                    <Form.Control.Feedback type="invalid">
-                        Enter your Phone Number!!
-                    </Form.Control.Feedback>
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="address">
-                    <Form.Label>Address</Form.Label>
-                    <Form.Control type="text" placeholder="Name" onChange={onChangeHandler} required/>
-                    <Form.Control.Feedback type="invalid">
-                        Enter your Address!!
-                    </Form.Control.Feedback>
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="password">
-                    <Form.Label>Password</Form.Label>
-                    <OverlayTrigger trigger="focus" placement="right" overlay={Popover()}>
-                        <Form.Control type="password" placeholder="Password" onChange={this.passwordOnchange} 
-                            minLength={6} pattern='^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,16}' required
-                        />
-                    </OverlayTrigger>
-                    <Form.Control.Feedback type="invalid">
-                        Enter your password!!
-                    </Form.Control.Feedback>
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="re-enter-password">
-                    <Form.Label>Confirm Password</Form.Label>
-                    <Form.Control type="password" placeholder="Re-enter Password" pattern={`^${auth.password}$`}  required />
-                    <Form.Control.Feedback type="invalid">
-                        Its not the same.
-                    </Form.Control.Feedback>
-                </Form.Group>
-                <Button variant="warning" type="submit">
-                    Submit
-                </Button>
-                <p>Already Have Account? <span className="anotherPage" onClick={() => {this.props.nav('/Login')}}>Login</span></p>
-            </Form>
+                    <Row>
+                        <Form.Group as={Col} className="mb-3" controlId="username">
+                            <Form.Label>Username</Form.Label>
+                            <Form.Control type="text" placeholder="Username" onChange={onChangeHandler} required/>
+                            <Form.Control.Feedback type="invalid">
+                                Enter your Username!!
+                            </Form.Control.Feedback>
+                        </Form.Group>
+                        <Form.Group as={Col} className="mb-3" controlId="name">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control type="text" placeholder="Name" onChange={onChangeHandler} required/>
+                            <Form.Control.Feedback type="invalid">
+                                Enter your Name!!
+                            </Form.Control.Feedback>
+                        </Form.Group>
+                    </Row>
+                    <Form.Group className="mb-3" controlId="email">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="example@email.com" onChange={onChangeHandler}   required />
+                        <Form.Control.Feedback type='invalid' >
+                        Enter your email like this : example@email.com
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                    <Row>
+                        <Form.Group as={Col} className="mb-3" controlId="password">
+                            <Form.Label>Password</Form.Label>
+                            <OverlayTrigger trigger="focus" placement="right" overlay={Popover()}>
+                                <Form.Control type="password" placeholder="Password" onChange={this.passwordOnchange} 
+                                    minLength={6} pattern='^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,16}' required
+                                />
+                            </OverlayTrigger>
+                            <Form.Control.Feedback type="invalid">
+                                Enter your password!!
+                            </Form.Control.Feedback>
+                        </Form.Group>
+                        <Form.Group as={Col} className="mb-3" controlId="re-enter-password">
+                            <Form.Label>Confirm Password</Form.Label>
+                            <Form.Control type="password" placeholder="Re-enter Password" pattern={`^${auth.password}$`}  required />
+                            <Form.Control.Feedback type="invalid">
+                                Its not the same.
+                            </Form.Control.Feedback>
+                        </Form.Group>
+                    </Row>
+                    <Row>
+                        <Form.Group as={Col} className="mb-3" controlId="phone_numbe">
+                            <Form.Label>Phone Nubmer</Form.Label>
+                            <Form.Control type="text" placeholder="Name" onChange={onChangeHandler} required/>
+                            <Form.Control.Feedback type="invalid">
+                                Enter your Phone Number!!
+                            </Form.Control.Feedback>
+                        </Form.Group>
+                        <Form.Group as={Col} className="mb-3" controlId="address">
+                            <Form.Label>Address</Form.Label>
+                            <Form.Control type="text" placeholder="Name" onChange={onChangeHandler} required/>
+                            <Form.Control.Feedback type="invalid">
+                                Enter your Address!!
+                            </Form.Control.Feedback>
+                        </Form.Group>
+                    </Row>
+                    <Button variant="warning" type="submit">
+                        Submit
+                    </Button>
+                    <p>Already Have Account? <span className="anotherPage" onClick={() => {this.props.nav('/Login')}}>Login</span></p>
+                </Form>
             </Container>
         )
     }
